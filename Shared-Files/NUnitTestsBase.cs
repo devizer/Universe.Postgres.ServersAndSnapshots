aa540105f406272c23c1c4136125241afccd8873
devizer <devizer@users.github.io>
2023-01-14 07:01:29 +0200

    Mac Os Tests

--- Shared-Files/NUnitTestsBase.cs
+++ Shared-Files/NUnitTestsBase.cs
@@ -10,19 +10,19 @@ using Universe.Postgres.ServersAndSnapshots;
 
 namespace Universe.NUnitTests
 {
-	public class NUnitTestsBase
-	{
-		public static bool IsTravis => Environment.GetEnvironmentVariable("TRAVIS") == "true";
+    public class NUnitTestsBase
+    {
+        public static bool IsTravis => Environment.GetEnvironmentVariable("TRAVIS") == "true";
 
-		protected static TextWriter OUT;
-		private Stopwatch StartAt;
-		private CpuUsage.CpuUsage? _CpuUsage_OnStart;
-		private int TestCounter = 0, TestClassCounter = 0;
+        protected static TextWriter OUT;
+        private Stopwatch StartAt;
+        private CpuUsage.CpuUsage? _CpuUsage_OnStart;
+        private int TestCounter = 0, TestClassCounter = 0;
         private int TestCounterStorage = 0;
         private static int TestClassCounterStorage = 0;
 
-		
-		Action OnDisposeList = () => { };
+
+        Action OnDisposeList = () => { };
 
         private int OnDisposeCounter = 0;
         protected void OnDispose(string title, Action action)
@@ -41,10 +41,10 @@ namespace Universe.NUnitTests
                 }
             };
         }
-        
+
         protected void OnDispose(Action action)
         {
-			OnDispose($"Dispose Action #{Interlocked.Increment(ref OnDisposeCounter)}", action);
+            OnDispose($"Dispose Action #{Interlocked.Increment(ref OnDisposeCounter)}", action);
         }
 
         protected void OnDisposeSilent(Action action)
@@ -55,118 +55,120 @@ namespace Universe.NUnitTests
 
 
         [SetUp]
-		public void BaseSetUp()
-		{
-			TestConsole.Setup();
-			Environment.SetEnvironmentVariable("SKIP_FLUSHING", null);
-			StartAt = Stopwatch.StartNew();
-			_CpuUsage_OnStart = GetCpuUsage();
-			TestCounter = Interlocked.Increment(ref TestCounterStorage);
+        public void BaseSetUp()
+        {
+            TestConsole.Setup();
+            Environment.SetEnvironmentVariable("SKIP_FLUSHING", null);
+            StartAt = Stopwatch.StartNew();
+            _CpuUsage_OnStart = GetCpuUsage();
+            TestCounter = Interlocked.Increment(ref TestCounterStorage);
 
-			var testClassName = TestContext.CurrentContext.Test.ClassName;
-			testClassName = testClassName.Split('.').LastOrDefault();
-			Console.WriteLine($"#{TestClassCounter}.{TestCounter} {{{TestContext.CurrentContext.Test.Name}}} @ {testClassName} starting...");
-		}
+            var testClassName = TestContext.CurrentContext.Test.ClassName;
+            testClassName = testClassName.Split('.').LastOrDefault();
+            Console.WriteLine($"#{TestClassCounter}.{TestCounter} {{{TestContext.CurrentContext.Test.Name}}} @ {testClassName} starting...");
+        }
 
-		private CpuUsage.CpuUsage? GetCpuUsage()
-		{
-			try
-			{
-				// return LinuxResourceUsage.GetByThread();
-				return CpuUsage.CpuUsage.Get(CpuUsageScope.Thread);
-			}
-			catch
-			{
-			}
+        private CpuUsage.CpuUsage? GetCpuUsage()
+        {
+            try
+            {
+                // return LinuxResourceUsage.GetByThread();
+                return CpuUsage.CpuUsage.Get(CpuUsageScope.Thread);
+            }
+            catch
+            {
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		[TearDown]
-		public void BaseTearDown()
-		{
-			var elapsed = StartAt.Elapsed;
-			var cpuUsage = "";
-			if (_CpuUsage_OnStart.HasValue)
-			{
-				var onEnd = GetCpuUsage();
-				if (onEnd != null)
-				{
-					var delta = CpuUsage.CpuUsage.Substruct(onEnd.Value, _CpuUsage_OnStart.Value);
-					var user = delta.UserUsage.TotalMicroSeconds / 1000d;
-					var kernel = delta.KernelUsage.TotalMicroSeconds / 1000d;
-					var perCents = (user + kernel) / 1000d / elapsed.TotalSeconds;
-					cpuUsage = $" (cpu: {perCents * 100:f0}%, {user + kernel:n3} = {user:n3} [user] + {kernel:n3} [kernel] milliseconds)";
-				}
-			}
+        [TearDown]
+        public void BaseTearDown()
+        {
+            var elapsed = StartAt.Elapsed;
+            var cpuUsage = "";
+            if (_CpuUsage_OnStart.HasValue)
+            {
+                var onEnd = GetCpuUsage();
+                if (onEnd != null)
+                {
+                    var delta = CpuUsage.CpuUsage.Substruct(onEnd.Value, _CpuUsage_OnStart.Value);
+                    var user = delta.UserUsage.TotalMicroSeconds / 1000d;
+                    var kernel = delta.KernelUsage.TotalMicroSeconds / 1000d;
+                    var perCents = (user + kernel) / 1000d / elapsed.TotalSeconds;
+                    cpuUsage = $" (cpu: {perCents * 100:f0}%, {user + kernel:n3} = {user:n3} [user] + {kernel:n3} [kernel] milliseconds)";
+                }
+            }
 
-			Console.WriteLine(
-				$"#{TestClassCounter}.{TestCounter} {{{TestContext.CurrentContext.Test.Name}}} >{TestContext.CurrentContext.Result.Outcome.Status.ToString().ToUpper()}< in {elapsed}{cpuUsage}{Environment.NewLine}");
+            Console.WriteLine(
+                $"#{TestClassCounter}.{TestCounter} {{{TestContext.CurrentContext.Test.Name}}} >{TestContext.CurrentContext.Result.Outcome.Status.ToString().ToUpper()}< in {elapsed}{cpuUsage}");
 
             var copy = OnDisposeList;
             OnDisposeList = () => { };
             copy();
+
+            Console.WriteLine("");
         }
 
-		[OneTimeSetUp]
-		public void BaseOneTimeSetUp()
+        [OneTimeSetUp]
+        public void BaseOneTimeSetUp()
         {
             TestClassCounter = Interlocked.Increment(ref TestClassCounterStorage);
             TestConsole.Setup();
-		}
+        }
 
-		[OneTimeTearDown]
-		public void BaseOneTimeTearDown()
-		{
-			// nothing todo
-		}
+        [OneTimeTearDown]
+        public void BaseOneTimeTearDown()
+        {
+            // nothing todo
+        }
 
-		protected static bool IsDebug
-		{
-			get
-			{
+        protected static bool IsDebug
+        {
+            get
+            {
 #if DEBUG
                 return true;
 #else
 				return false;
 #endif
-			}
-		}
+            }
+        }
 
-		public class TestConsole
-		{
-			private static bool Done = false;
+        public class TestConsole
+        {
+            private static bool Done = false;
 
-			public static void Setup()
-			{
-				if (!Done)
-				{
-					Done = true;
-					Console.SetOut(new TW());
-				}
-			}
+            public static void Setup()
+            {
+                if (!Done)
+                {
+                    Done = true;
+                    Console.SetOut(new TW());
+                }
+            }
 
-			private class TW : TextWriter
-			{
-				public override Encoding Encoding { get; }
+            private class TW : TextWriter
+            {
+                public override Encoding Encoding { get; }
 
-				public override void WriteLine(string value)
-				{
-//                    TestContext.Progress.Write(string.Join(",", value.Select(x => ((int)x).ToString("X2"))) );
-//                    if (value.Length > Environment.NewLine.Length && value.EndsWith(Environment.NewLine))
-//                        value = value.Substring(0, value.Length - Environment.NewLine.Length);
+                public override void WriteLine(string value)
+                {
+                    //                    TestContext.Progress.Write(string.Join(",", value.Select(x => ((int)x).ToString("X2"))) );
+                    //                    if (value.Length > Environment.NewLine.Length && value.EndsWith(Environment.NewLine))
+                    //                        value = value.Substring(0, value.Length - Environment.NewLine.Length);
 
 
-					try
-					{
-						TestContext.Progress.WriteLine(value);
-						// TestContext.Error.WriteLine(value); // .WriteLine();
-					}
-					catch
-					{
-					}
-				}
-			}
-		}
-	}
+                    try
+                    {
+                        TestContext.Progress.WriteLine(value);
+                        // TestContext.Error.WriteLine(value); // .WriteLine();
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+    }
 }
