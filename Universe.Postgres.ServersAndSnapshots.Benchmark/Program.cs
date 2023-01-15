cd9698e14c52397f0338763187eb54508025c770
devizer <devizer@users.github.io>
2023-01-15 03:08:37 +0200

    Added benchmark job

---
+++ Universe.Postgres.ServersAndSnapshots.Benchmark/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Universe.Postgres.ServersAndSnapshots.Benchmark
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Summary summary = BenchmarkRunner.Run<ServerBenchmark>();
+        }
+    }
+
+    public class ServerBenchmark
+    {
+        private ServerBinaries _server;
+        List<string> Directories = new List<string>(10000);
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _server = PostgresServerDiscovery.GetServers().OrderByDescending(x => x.Version).FirstOrDefault();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            foreach (var directory in Directories)
+            {
+                try
+                {
+                    Directory.Delete(directory, true);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        [Benchmark]
+        public void InitDb()
+        {
+            var dataPath = Path.Combine(TestUtils.RootWorkFolder, DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss-ffff"));
+            Directories.Add(dataPath);
+
+            PostgresInstanceOptions options = new PostgresInstanceOptions()
+            {
+                DataPath = dataPath,
+                ServerPort = Interlocked.Increment(ref TestUtils.Port),
+            };
+
+            PostgresServerManager.CreateServerInstance(_server, options);
+        }
+    }
+
+    internal class TestUtils
+    {
+        public static int Port = 5432;
+
+        public static string RootWorkFolder
+        {
+            get
+            {
+
+                var ret = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+
+                    ? Path.Combine(
+                        Path.GetPathRoot(Environment.SystemDirectory),
+                        "Temp\\Postgres-Work-Folder")
+                    : "/tmp/Postgres-Work-Folder";
+
+                TryAndForget.Execute(() => Directory.CreateDirectory(ret));
+                return ret;
+            }
+        }
+    }
+}
