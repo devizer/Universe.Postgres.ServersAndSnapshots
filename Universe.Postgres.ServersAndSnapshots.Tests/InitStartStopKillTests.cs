e9fdb1ef4cec293cd19baa4b317cee709b838b0e
devizer <devizer@users.github.io>
2023-01-16 08:29:52 +0200

    Dix conditions, added timeout for tests

--- Universe.Postgres.ServersAndSnapshots.Tests/InitStartStopKillTests.cs
+++ Universe.Postgres.ServersAndSnapshots.Tests/InitStartStopKillTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Npgsql;
 using NUnit.Framework;
 using Universe.NUnitTests;
