3660bb84ef83db718f1a98aa5b55bb0f37768188
devizer <devizer@users.github.io>
2023-01-25 20:54:15 +0200

    Added Encoding for NpgsqlLocaleExtensions

--- Universe.NpglExtensions/NpgsqlLocaleExtensions.cs
+++ Universe.NpglExtensions/NpgsqlLocaleExtensions.cs
@@ -10,16 +10,17 @@ namespace Universe.NpglExtensions
             public string DbName { get; set; }
             public string Collate { get; set; }
             public string CType { get; set; }
+            public string Encoding { get; set; }
 
             public override string ToString()
             {
-                return $"{nameof(DbName)}='{DbName}', {nameof(Collate)}='{Collate}', {nameof(CType)}='{CType}'";
+                return $"{nameof(DbName)}: {DbName}, {nameof(Collate)}: {Collate}, {nameof(CType)}: {CType}, {nameof(Encoding)}: {Encoding}";
             }
         }
 
         public static DbLocale GetCurrentDatabaseLocale(this IDbConnection connection)
         {
-            const string sql = "Select datname As DbName, datcollate As Collate, datctype As CType FROM pg_database where datname = current_database();";
+            const string sql = "Select datname As DbName, datcollate As Collate, datctype As CType, pg_encoding_to_char(encoding) As Encoding FROM pg_database where datname = current_database();";
             return connection.QueryFirst<DbLocale>(sql);
         }
     }
