1a175e09600a1c57fbed84fbd93908381da27518
devizer <devizer@users.github.io>
2023-02-02 19:13:11 +0200

    Fix PostgresServerDiscovery for macports v8

---
+++ build-scripts/Install-MacPorts.sh
@@ -0,0 +1,29 @@
+function get_darwin_version_major() {
+  if [[ "$(uname -s)" == Darwin ]]; then
+    echo "$(sysctl -n kern.osrelease | awk -F'.' '{print $1}')"
+  fi
+}
+
+echo "get_darwin_version_major is [$(get_darwin_version_major)]"
+v="$(get_darwin_version_major)"
+case $v in
+  22) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-13-Ventura.pkg";;
+  21) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-12-Monterey.pkg";;
+  20) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-11-BigSur.pkg";;
+  19) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.15-Catalina.pkg";;
+  18) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.14-Mojave.pkg";;
+  17) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.13-HighSierra.pkg";;
+  16) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.12-Sierra.pkg";;
+  15) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.11-ElCapitan.pkg";;
+  14) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.10-Yosemite.pkg";;
+  13) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.9-Mavericks.pkg";;
+  12) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.8-MountainLion.pkg";;
+  11) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.7-Lion.pkg";;
+  10) macports_pkg="https://github.com/macports/macports-base/releases/download/v2.8.1/MacPorts-2.8.1-10.6-SnowLeopard.pkg";;
+  *)  macports_pkg="";;
+esac
+echo "macports_pkg=[$macports_pkg]"
+
+curl -kSL -o MacPorts.pkg "$macports_pkg" || curl -kSL -o MacPorts.pkg "$macports_pkg"
+time sudo installer -pkg MacPorts.pkg -target / # -verbose
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
