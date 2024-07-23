ecb973d5ebb1bbd4e653bc474bd1eff42d057bc0
devizer <devizer@users.github.io>
2024-07-23 12:18:46 +0300

    v9: Test All Windows Portable (both tiny and full), aria2c ***NO_CI***

--- build-scripts/Download-All-Windows-Portable/dist/Postgres-Version-Manager.ps1
+++ build-scripts/Download-All-Windows-Portable/dist/Postgres-Version-Manager.ps1
@@ -1195,6 +1195,6 @@ else {
 # try psql
 Say "Finally, query newly installed PostgreSQL"
 $ENV:PGPASSWORD="$Password"
-echo "SELECT 'User is [' || current_user || ']. Database is [' || current_database() || ']. Timezone is [' || current_setting('TIMEZONE') || ']. Server is [' || setting || ']' FROM pg_settings WHERE name = 'server_version'; SELECT 'MAX Connections is ' || setting FROM pg_settings WHERE name = 'max_connections';" | & "$psql" "-t" "-h" localhost "-p" $Port "-U" $Admin postgres
+echo "SELECT 'User is [' || current_user || ']. Database is [' || current_database() || ']. Timezone is [' || current_setting('TIMEZONE') || ']. Server is [' || setting || ']. Encoding is [' || pg_client_encoding() || ']' FROM pg_settings WHERE name = 'server_version'; SELECT 'MAX Connections is ' || setting FROM pg_settings WHERE name = 'max_connections';" | & "$psql" "-t" "-h" localhost "-p" $Port "-U" $Admin postgres
 if (-not $?) { Write-Host "Error querying newly created postgresql server" -ForeGroundColor Red; }
 
